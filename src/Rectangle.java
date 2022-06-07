7e63d141111ebcbff815f870287ef100c349a1f7
Mehul Chopra <mchopra@abico.com>
2022-06-07 15:08:25 +0530

    added area rectangle function

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -4,11 +4,17 @@ public class Rectangle {
         return 2 * (length + breadth);
     }
 
+    static int areaRectangle(int length, int breadth) {
+        return length * breadth;
+    }
+
     public static void main(String[] args) {
         var l1 = 8;
         var b1 = 4;
 
         var p1 = perimeterRectangle(l1, b1);
         System.out.println(p1);
+
+        System.out.println(areaRectangle(l1, b1));
     }
 }
